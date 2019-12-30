72bf61fbc250b09bc1d077c21ca025dcf2de96bd
olku-kun <kraoly@gmail.com>
2019-12-30 19:37:57 +0200

    adding  date function to js

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ function clock() {
     let hours = document.querySelector('#hours');
     let minutes = document.querySelector('#minutes');
     let seconds = document.querySelector('#seconds');
+    let day = document.querySelector('#day');
+    let month = document.querySelector('#month');
+    let year = document.querySelector('#year');
     hours.innerHTML = new Date().getHours();
     minutes.innerHTML = new Date().getMinutes();
     seconds.innerHTML = new Date().getSeconds();
+    day.innerHTML = new Date().getDate();
+    year.innerHTML = new Date().getFullYear();
+//Month number to month name conversion
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let i = new Date().getMonth();
+    month.innerHTML =  monthNames[i] ;
 }
+
+
+
 let interval = setInterval(clock, 1000);
